837cd9f295930cc271787d94de9f5da44c4f53cc
Gali Bhaskar <luciferbhaskar@gmail.com>
2024-08-12 06:27:46 -0700

    Updated Routes for Vercel

--- back-end/index.js
+++ back-end/index.js
@@ -1,20 +1,24 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import config from './config.js';
-import bannerRoutes from './routes/banner.js';
+import express, { json } from "express";
+import cors from "cors";
+import config from "./config.js";
+import bannerRoutes from "./routes/banner.js";
 import "./cron-jobs/index.js";
 
 const app = express();
 
 const port = process.env.API_PORT || config.API_PORT || 3001;
 
-const host = process.env.HOSTNAME || config.hostname || 'localhost';
+const host = process.env.HOSTNAME || config.hostname || "localhost";
 
 app.use(cors());
 
 app.use(json());
 
-app.use('/api/banner', bannerRoutes);
+app.get("/api", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/api/banner", bannerRoutes);
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
